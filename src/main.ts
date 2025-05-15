ba50d7958ef3ba17f5685df19ed0e10ddc8955b5
Marc Archer <marc.archer@premiumnumbers.es>
2025-05-15 15:08:22 +0200

    feat: refactor products microservice to use message patterns and improve product handling

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('main')
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      }
+    }
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,7 +23,8 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(envs.port ?? 3000);
-  logger.log(`Application is running on: ${await app.getUrl()}`);
+  await app.listen();
+
+  logger.log(`Products microservice is running on: ${envs.port}`);
 }
 bootstrap();
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from 'generated/prisma';
@@ -22,13 +22,14 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   async findAll( paginationDto: PaginationDto) {
     const { page = 1, limit = 10 } = paginationDto;
 
-    const totalPages = await this.product.count();
+    const totalPages = await this.product.count({ where: { available: true } });
     const lastPage = Math.ceil(totalPages / limit);
 
     return {
       data: await this.product.findMany({
         skip: (page - 1) * limit,
-        take: limit
+        take: limit,
+        where: { available: true },
       }),
       meta: {
         total: totalPages,
@@ -38,15 +39,41 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: { id, available: true }
+    })
+
+    if ( !product ) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+    const {id: __, ...data } = updateProductDto;
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id },
+      data: data
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    await this.findOne(id);
+
+/*     return this.product.delete({
+      where: { id }
+    }); */
+
+    const product = await this.product.update({
+      where: { id },
+      data: { available: false }
+    })
+
+    return product;
   }
 }
